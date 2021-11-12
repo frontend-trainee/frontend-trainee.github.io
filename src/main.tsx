a1975f2ec97541a721d1d6bb789cf3e527860559
MleMoe <i@mle.moe>
2021-11-13 00:40:27 +0800

    :construction: test micro frontend

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+import { registerMicroApps, start } from 'qiankun';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById('root-0')
+);
+
+registerMicroApps([
+  {
+    name: 'viteApp', // app name registered
+    entry: '//mlemoe.github.io/frontend-mentor/',
+    container: '#child-container',
+    activeRule: '/yourActiveRule',
+  },
+  {
+    name: 'vueApp',
+    entry: { scripts: ['//localhost:7100/main.js'] },
+    container: '#child-container',
+    activeRule: '/yourActiveRule2',
+  },
+]);
+
+start();

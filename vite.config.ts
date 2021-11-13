833ad88e4c1d246259ca4ee7bdb9fccf075cbe8f
MleMoe <i@mle.moe>
2021-11-13 18:36:47 +0800

    :coffin: del alias

--- vite.config.ts
+++ vite.config.ts
@@ -5,20 +5,4 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   // base: '/micro-frontend-base/',
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@/': './src',
-      '@/config': './src/config',
-      '@/components': './src/components',
-      '@/styles': './src/styles',
-      '@/utils': './src/utils',
-      '@/common': './src/common',
-      '@/assets': './src/assets',
-      '@/pages': './src/pages',
-      '@/routes': './src/routes',
-      '@/layouts': './src/layouts',
-      '@/hooks': './src/hooks',
-      '@/store': './src/store',
-    },
-  },
 });
